c95fe2f165f95bed8cfb4ef870ed164c4c89259a
giannirandone <info.randonegianni@gmail.com>
2019-08-02 00:03:40 +0200

    Es wurden den Unterpunkten in der NavigationBar Fragements zugeordnet.

--- app/src/main/java/com/example/giannirandone/applicationgiannirandone/MainActivity.java
+++ app/src/main/java/com/example/giannirandone/applicationgiannirandone/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.giannirandone.applicationgiannirandone;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -20,13 +23,45 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AnschreibenFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_anschreiben);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_anschreiben:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AnschreibenFragment()).commit();
+                break;
+            case R.id.nav_persoenlichedaten:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PersoenlicheDatenFragment()).commit();
+                break;
+            case R.id.nav_karriere:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new KarriereFragment()).commit();
+                break;
+            case R.id.nav_interessenundperson:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InteressenUndPersonFragment()).commit();
+                break;
+            case R.id.nav_kontakt:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new KontaktFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     @Override
     public void onBackPressed()
     {
